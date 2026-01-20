fba8b42e7b3d8384a392463138ee5e0dbfdf8d2a
snozzono <164924936+snozzono@users.noreply.github.com>
2026-01-20 16:41:06 -0300

    1.8

--- part 1/src/App.jsx
+++ part 1/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react'
 
-const Header = ({ text }) => 
-<h1>{text}</h1>
+const Header = ({ text }) => {
+  return <h1>{text}</h1>
+}
 
-const clicker = (setter, value) => () => setter(value + 1)
+const clicker = (setter, value) => {
+  return () => setter(value + 1)
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
+  return (
+    <div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {isNaN(average) ? 0 : average}</div>
+      <div>positive {isNaN(positive) ? 0 : positive} %</div> 
+    </div>
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = (good / all) * 100
 
   return (
     <div>
@@ -22,12 +39,7 @@ const App = () => {
       <button onClick={clicker(setBad, bad)}>bad</button>
 
       <Header text="statistics" />
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {isNaN(average) ? 0 : average}</div>
-      <div>positive {isNaN(positive) ? 0 : positive} %</div> 
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
